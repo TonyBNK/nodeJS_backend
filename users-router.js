69e6f42ecfc345741de3513dc3de18c7e394ce95
TonyBNK <borisenk-anton@yandex.ru>
2021-11-22 16:28:11 +0300

    Added get user by id functionality.

--- users-router.js
+++ users-router.js
@@ -1,5 +1,6 @@
 const {addUser, getUsers} = require("./repository");
 const express = require("express");
+const {use} = require("express/lib/router");
 
 
 const router = express.Router();
@@ -9,13 +10,23 @@ router
         const users = await getUsers();
         res.send(JSON.stringify(users));
     })
+    .get('/:id', async (req, res) => {
+        const users = await getUsers();
+        const userId = req.params.id;
+        debugger
+        const user = users.find(u => u.id === +userId);
+        if (user) {
+            res.send(JSON.stringify(user));
+        } else {
+            res.send(404);
+        }
+    })
     .post('/', async (req, res) => {
         await addUser('Tobi');
         res.send(JSON.stringify({success: true}));
     })
-    .use((req, res, next) => {
-        console.log(`Time: ${Date.now()}`);
-        next();
+    .use((req, res) => {
+        res.send(404);
     });
 
 module.exports = router;
