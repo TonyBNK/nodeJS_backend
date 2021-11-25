831f9b506324ae9a48a654280e2c4bca99834c1a
TonyBNK <borisenk-anton@yandex.ru>
2021-11-25 18:50:22 +0300

    Updated for simple deploying.

--- index.js
+++ index.js
@@ -5,31 +5,30 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 
-async function main() {
-    await mongoose.connect('mongodb://localhost:27017/nodeJS');
-}
-
-main().catch(err => console.log(err));
+// async function main() {
+//     await mongoose.connect('mongodb://localhost:27017/nodeJS');
+// }
+//
+// main().catch(err => console.log(err));
 
 const app = express();
-const port = 3010;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app
-    .get('/', (req, res) => {
-        res.send(`<h1>Home</h1>`);
-    })
+    // .get('/', (req, res) => {
+    //     res.send(`<h1>Home</h1>`);
+    // })
     .get('/tasks', (req, res) => {
         res.send(`<h1>Tasks</h1>`);
     })
-    .use('/users', users)
+    //.use('/users', users)
     .use((req, res) => {
         res.send(404);
     });
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(3010, () => {
+    console.log(`Example app listening at port ${process.env.PORT}`);
 });
